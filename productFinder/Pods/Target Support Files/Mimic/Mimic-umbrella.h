b5d99bf69fc08098d0cc954f7ef942edefc8c760
Rocío Barramuño <rocio.barramuno@gmail.com>
2023-11-03 02:31:46 -0300

    build(pods): Delete Alamofire and add Mimic

--- productFinder/Pods/Target Support Files/Mimic/Mimic-umbrella.h
+++ productFinder/Pods/Target Support Files/Mimic/Mimic-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double AlamofireVersionNumber;
-FOUNDATION_EXPORT const unsigned char AlamofireVersionString[];
+FOUNDATION_EXPORT double MimicVersionNumber;
+FOUNDATION_EXPORT const unsigned char MimicVersionString[];
 
