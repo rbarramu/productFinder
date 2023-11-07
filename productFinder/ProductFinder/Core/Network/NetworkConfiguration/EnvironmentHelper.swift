e41990abc783d0e0bbdab4112febab2954a17e32
Rocío Barramuño <rocio.barramuno@gmail.com>
2023-11-06 21:03:03 -0300

    chore: Change environment to release

--- productFinder/ProductFinder/Core/Network/NetworkConfiguration/EnvironmentHelper.swift
+++ productFinder/ProductFinder/Core/Network/NetworkConfiguration/EnvironmentHelper.swift
@@ -1,6 +1,6 @@
 enum EnvironmentHelper {
     #if DEBUG
-        static var current: Environment = .stubbed
+        static var current: Environment = .release
     #elseif ADHOC
         static var current: Environment = .debug
     #else
